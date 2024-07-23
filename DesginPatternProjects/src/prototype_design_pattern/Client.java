30ad7519ad426e423087d0010d064fcc4ff3901f
MY <MY@host.docker.internal>
2024-07-23 12:12:47 +0530

    Perform Deep Copy in Prototype Design Pattern

--- DesginPatternProjects/src/prototype_design_pattern/Client.java
+++ DesginPatternProjects/src/prototype_design_pattern/Client.java
@@ -9,6 +9,7 @@ public class Client {
 		System.out.println("Original Object");
 		System.out.println(networkConnection);
 		System.out.println(networkConnection.hashCode());
+		
 	
 		
 		//We want new Object of network connection
@@ -18,12 +19,15 @@ public class Client {
 			
 		NetworkConnection networkConnection2 =(NetworkConnection) networkConnection.clone();
 		System.out.println("Clone Object 1");
-		System.out.println(networkConnection2);
-		System.out.println(networkConnection2.hashCode());
+	
 		NetworkConnection networkConnection3 =(NetworkConnection) networkConnection.clone();
 		System.out.println("Clone Object 2");
+		networkConnection.getDomains().remove(0);
+		System.out.println(networkConnection2);
+		System.out.println(networkConnection2.hashCode());
 		System.out.println(networkConnection3);
 		System.out.println(networkConnection3.hashCode());
+		System.out.println(networkConnection);
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
--- DesginPatternProjects/src/prototype_design_pattern/NetworkConnection.java
+++ DesginPatternProjects/src/prototype_design_pattern/NetworkConnection.java
@@ -1,14 +1,24 @@
 package prototype_design_pattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NetworkConnection implements Cloneable {
 	
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
+	NetworkConnection networkConnetion = new NetworkConnection();
+	networkConnetion.setIp(this.getIp());
+	networkConnetion.setData(this.getData());
 	
-		return super.clone();
+	for(String d: this.getDomains()) {
+		networkConnetion.getDomains().add(d);
+	}
+		return networkConnetion;
 	}
 	private String ip;
 	private String data;
+	private List<String> domains = new ArrayList<>(); 
 	
 	
 	
@@ -26,12 +36,24 @@ public class NetworkConnection implements Cloneable {
 	}
 	
 	
+	public List<String> getDomains() {
+		return domains;
+	}
+	public void setDomains(List<String> domains) {
+		this.domains = domains;
+	}
+	 
+	
 	@Override
 	public String toString() {
-		return "NetworkConnection [ip=" + ip + ", data=" + data + "]";
+		return "NetworkConnection [ip=" + ip + ", data=" + data + ", domains=" + domains + "]";
 	}
 	public void  loadImpData() throws InterruptedException {
 		this.data="Very very Complex and imp data";
+		domains.add("www.achingoyal.com");
+		domains.add("www.google.com");
+		domains.add("www.facebook.com");
+		domains.add("www.gmail.com");
 		Thread.sleep(5000);
 	}
 
